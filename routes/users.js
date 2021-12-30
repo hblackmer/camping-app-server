cfbc254b7937745d2ec3f1519e643c6b999bff61
Helene <hcblackmer@gmail.com>
2021-12-30 12:32:01 -0800

    Allow admins to access users documents.

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,11 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+    User.find()
+    .then(users => {
+      res.json(users);
+    })
 });
 
 router.post('/signup', (req, res) => {
