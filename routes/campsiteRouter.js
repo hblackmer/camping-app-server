b9815d6b71ec7c17c1f038a3d1d9d0e7ce6d5e38
Helene <hcblackmer@gmail.com>
2021-12-30 13:26:05 -0800

    Allow logged-in users to update or delete any comments that they themselves submitted.

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -168,27 +168,33 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
 .put(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
-        if (campsite && campsite.comments.id(req.params.commentId)) {
-            if (req.body.rating) {
-                campsite.comments.id(req.params.commentId).rating = req.body.rating;
+        if (req.user._id.equals(commentId.author._id)) {
+            if (campsite && campsite.comments.id(req.params.commentId)) {
+                if (req.body.rating) {
+                    campsite.comments.id(req.params.commentId).rating = req.body.rating;
+                }
+                if (req.body.text) {
+                    campsite.comments.id(req.params.commentId).text = req.body.text;
+                }
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+            } else if (!campsite) {
+                err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                err.status = 404;
+                return next(err);
+            } else {
+                err = new Error(`Comment ${req.params.commentId} not found`);
+                err.status = 404;
+                return next(err);
             }
-            if (req.body.text) {
-                campsite.comments.id(req.params.commentId).text = req.body.text;
-            }
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
-        } else if (!campsite) {
-            err = new Error(`Campsite ${req.params.campsiteId} not found`);
-            err.status = 404;
-            return next(err);
         } else {
-            err = new Error(`Comment ${req.params.commentId} not found`);
-            err.status = 404;
+            err = new Error(`You are not authorized to perform this operation!`);
+            err.status = 403;
             return next(err);
         }
     })
@@ -197,22 +203,28 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
 .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
-        if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).remove();
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
-        } else if (!campsite) {
-            err = new Error(`Campsite ${req.params.campsiteId} not found`);
-            err.status = 404;
-            return next(err);
+        if (req.user._id.equals(commentId.author._id)) {
+            if (campsite && campsite.comments.id(req.params.commentId)) {
+                campsite.comments.id(req.params.commentId).remove();
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+            } else if (!campsite) {
+                err = new Error(`Campsite ${req.params.campsiteId} not found`);
+                err.status = 404;
+                return next(err);
+            } else {
+                err = new Error(`Comment ${req.params.commentId} not found`);
+                err.status = 404;
+                return next(err);
+            }
         } else {
-            err = new Error(`Comment ${req.params.commentId} not found`);
-            err.status = 404;
+            err = new Error(`You are not authorized to perform this operation!`);
+            err.status = 403;
             return next(err);
         }
     })
